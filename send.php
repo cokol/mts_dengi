0412c6d7d9566b556c2e05feb8e2b39d89a539c3
cokol <cokolheaven@gmail.com>
2013-10-24 20:49:11 +0400

    Revert "last changes"
    This reverts commit dff23f00279f53442136d72f469d78ad498eb2a8.

--- send.php
+++ send.php
@@ -1,9 +1,39 @@
 <?php
-$body="Фамилия: ".$_POST['campaign_lastname']."\n";
-$body.="Имя: ".$_POST['campaign_name']."\n";
-$body.="Электронная почта: ".$_POST['campaign_email']."\n";
-$body.="Контактный телефон: ".$_POST['campaign_phone']."\n";
 
-//mail("example@example.com", "Заявка на карту", $body);
+function getRealIpAddr()
+{
+  if (!empty($_SERVER['HTTP_CLIENT_IP']))
+  {
+    $ip=$_SERVER['HTTP_CLIENT_IP'];
+  }
+  elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+  {
+    $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+  }
+  else
+  {
+    $ip=$_SERVER['REMOTE_ADDR'];
+  }
+  return $ip;
+}
+
+//$_POST = str_replace ("'", "", $_POST);
+if($_POST["request_lastname"] && $_POST["request_name"] && $_POST["request_email"] && $_POST["request_phone"]){
+  //подключимся к базе
+  mysql_connect('localhost', 'dengi_mts_dbu', 'dengi_mts_360d');    \
+  mysql_select_db('dengi_mts');
+  mysql_query("SET NAMES utf8");
+  $query = "INSERT INTO `requests` VALUES('',NOW(),'".getRealIpAddr()."','".mysql_real_escape_string($_POST["request_name"])."','".mysql_real_escape_string($_POST["request_lastname"])."','".mysql_real_escape_string($_POST["request_email"])."','".mysql_real_escape_string($_POST["request_phone"])."')";
+  mysql_query($query);
+}
+
+
+//var_dump($_POST);
+$body="Фамилия: ".$_POST['request_lastname']."\n";
+$body.="Имя: ".$_POST['request_name']."\n";
+$body.="Электронная почта: ".$_POST['request_email']."\n";
+$body.="Контактный телефон: ".$_POST['request_phone']."\n";
+
+//mail("disco3000@gmail.com", "Заявка на карту", $body);
 
 ?>
